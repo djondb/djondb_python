b720c2663d1a246bebefae6c9485a9e93b224a3b
Cross <cross@djondb.com>
2016-08-29 19:44:06 +0200

    new version refreshed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup.py file for djondb python driver
 from distutils.core import setup, Extension
 
 setup (name = 'pydjondb',
-		version = '3.5.608293',
+		version = '3.5.60830',
 		author      = "Cross",
 		author_email = "cross@djondb.com",
 		license = "GPL",
