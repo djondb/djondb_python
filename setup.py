900c45077809d4ca6d12d3b321dfefb8955b76fb
Cross <cross@djondb.com>
2016-12-28 08:29:46 -0500

    Added support to lists when inserting and updating

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup.py file for djondb python driver
 from distutils.core import setup, Extension
 
 setup (name = 'pydjondb',
-		version = '3.5.612280',
+		version = '3.5.612281',
 		author      = "Cross",
 		author_email = "cross@djondb.com",
 		license = "GPL",
